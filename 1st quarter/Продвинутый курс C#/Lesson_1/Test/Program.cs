a4551c0f9e3039873ecfe23f87b2185438819816
babarobat <36125153+babarobat@users.noreply.github.com>
2018-10-02 08:58:46 +0300

    181002

---
+++ 1st quarter/Продвинутый курс C#/Lesson_1/Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(-5, 5);
+            Vector v2 = (Vector)10;
+            Vector v3 = v1 + v2;
+
+            Console.WriteLine($"v1.x={v1.X} v1.y={v1.Y}");
+            Console.WriteLine($"v2.x={v2.X} v2.y={v2.Y}");
+            Console.WriteLine($"(v1+v2):{v3}");
+            Console.WriteLine($"-(v1+v2):{-v3}");
+            Console.ReadKey();
+        }
+    }
+}
