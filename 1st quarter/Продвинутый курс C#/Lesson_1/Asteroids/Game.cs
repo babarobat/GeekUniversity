86e929e287433a28571ad3cf42a1d1c12f8ee47b
babarobat <36125153+babarobat@users.noreply.github.com>
2018-10-04 08:55:28 +0300

    181004

---
+++ 1st quarter/Продвинутый курс C#/Lesson_1/Asteroids/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// Основной класс для отрисовки графики игры
+    /// </summary>
+    static class GameGraphics
+    {
+        /// <summary>
+        /// Расширение с методами для графических буферов для двойной буферизации
+        /// </summary>
+        private static BufferedGraphicsContext _context;
+        /// <summary>
+        /// Буфер
+        /// </summary>
+        public static BufferedGraphics Buffer;
+        /// <summary>
+        /// Ширина экрана
+        /// </summary>
+        public static int Width { get; set;}
+        /// <summary>
+        /// Высота экрана
+        /// </summary>
+        public static int Height {get;set;}
+
+        static GameGraphics() { }
+
+        /// <summary>
+        /// Инициализация графики
+        /// </summary>
+        /// <param name="form">Форма для инициализации</param>
+        public static void Init(Form form)
+        {
+            Graphics graphics = form.CreateGraphics();           
+            _context = BufferedGraphicsManager.Current;
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            Buffer = _context.Allocate(graphics, new Rectangle(0, 0, Width, Height));
+        }
+        /// <summary>
+        /// Отрисовка графики
+        /// </summary>
+        public static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
+        }
+    }
+}
---
+++ 1st quarter/Продвинутый курс C#/Lesson_1/Asteroids/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asteroids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form GameForm = new Form()
+            {
+                Width = Screen.PrimaryScreen.Bounds.Width,
+                Height = Screen.PrimaryScreen.Bounds.Height
+            };
+            GameGraphics.Init(GameForm);
+            GameGraphics.Draw();
+            GameForm.Show();
+            Application.Run(GameForm);
+
+        }
+    }
+}
