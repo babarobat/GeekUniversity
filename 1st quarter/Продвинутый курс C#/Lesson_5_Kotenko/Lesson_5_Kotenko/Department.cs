496df0ce2beeed7c41d7c38710818eb2896ea976
babarobat <36125153+babarobat@users.noreply.github.com>
2018-10-31 19:00:32 +0300

    181031

---
+++ 1st quarter/Продвинутый курс C#/Lesson_5_Kotenko/Lesson_5_Kotenko/Department.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_Kotenko
+{
+    /// <summary>
+    /// Содержит параметры и логику департамента
+    /// </summary>
+    class Department : IEquatable<Department>
+    {
+        /// <summary>
+        /// статический параметр для создания уникального имени
+        /// </summary>
+        public static int Index { get; private set; }
+        ~Department()
+        {
+            Index--;
+        }
+        /// <summary>
+        /// Имя работника.
+        /// Если ввести пустую строку присвоит стандартное имя
+        /// </summary>
+        public string Name
+        {
+            get { return Name; }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    Name = value;
+                }
+                else
+                {
+                    Name = "Simple Department Name " + Index;
+                }
+            }
+        }
+        /// <summary>
+        /// Коллекция сотрудников департамента
+        /// </summary>
+        public ObservableCollection<Employee> Employees { get; private set; }
+        public Department()
+        {
+            Index++;
+            Employees = new ObservableCollection<Employee>();
+        }
+        /// <summary>
+        /// Создает департамент и заполняет его случайными сотрудниками
+        /// </summary>
+        public Department GetRandomDepartment()
+        {
+            Department tmpDep = new Department();
+            tmpDep.Name = "Simple Department Name " + Index;
+            Index++;
+            for (int i = 0; i < Util.GetRandom(5, 20); i++)
+            {
+                Employees.Add(Employee.GetRandomEmployee());
+            }
+            return tmpDep;
+        }
+        /// <summary>
+        /// Создает департамент с заданными параметрами
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="sel">Зарплата</param>
+        public Department(string name, ObservableCollection<Employee> employees)
+        {
+            Name = name;
+            Employees = employees;
+        }
+        /// <summary>
+        /// Сравнение двух департаментов на равенство
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Department other)
+        {
+
+            if (other.Name != Name)
+            {
+                return false;
+            }
+            else if (other.Employees.Count != Employees.Count)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < other.Employees.Count; i++)
+                {
+                    if (!other.Employees[i].Equals(Employees[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ 1st quarter/Продвинутый курс C#/Lesson_5_Kotenko/Lesson_5_Kotenko/Employee.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_Kotenko
+{
+    /// <summary>
+    /// Содержит параметры и логику сотрудника
+    /// </summary>
+    class Employee :IEquatable<Employee>
+    {
+        /// <summary>
+        /// статический параметр для создания уникального имени
+        /// </summary>
+        public static int Index { get; private set; }
+        ~Employee()
+        {
+            Index--;
+        }
+        /// <summary>
+        /// Имя работника.
+        /// Если ввести пустую строку присвоит стандартное имя
+        /// </summary>
+        public string Name
+        {
+            get { return Name; }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    Name = value;
+                }
+                else
+                {
+                    Name = "Simple Employee Name " + Index;
+                }
+            }
+        }
+        /// <summary>
+        /// Возраст сотрудника. Не может быть меньше 0
+        /// </summary>
+        public int Age
+        {
+            get { return Age; }
+            set
+            {
+                if (value >0)
+                {
+                    Age = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Зарплата сотрудника. Не может быть меньше 0
+        /// </summary>
+        public int Sel
+        {
+            get { return Sel; }
+            set
+            {
+                if (value > 0)
+                {
+                    Sel = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Employee()
+        {
+            Index++;
+        }
+        /// <summary>
+        /// Возвращает сотрудника с рандомными параметрами
+        /// </summary>
+        public static Employee GetRandomEmployee()
+        {
+            return new Employee()
+            {
+                Name = "Simple Employee Name " + Index,
+                Age = Util.GetRandom(20, 50),
+                Sel = Util.GetRandom(40, 150) * 1000
+            };
+        }
+        /// <summary>
+        /// Создает сотрудника с заданными параметрами
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="sel">Зарплата</param>
+        public Employee(string name, int age, int sel)
+        {
+            Name = name;
+            Age = age;
+            Sel = sel;
+        }
+        /// <summary>
+        /// Сравнение двух сотрудников на равенство
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Employee other)
+        {
+            return other.Name == Name && 
+                   other.Age == Age &&
+                   other.Sel == Sel;
+        }
+    }
+}
