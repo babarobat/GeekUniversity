cbf51f05f33c01ad3159336926f623516daefcea
babarobat <36125153+babarobat@users.noreply.github.com>
2018-11-05 18:53:59 +0300

    123

---
+++ 1st quarter/Продвинутый курс C#/Lesson_6_Kotenko/Lesson_5_Kotenko/Company.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections;
+
+namespace Lesson_5_Kotenko
+{
+    /// <summary>
+    /// Логика и свойства компании
+    /// </summary>
+    public class Company:IEnumerable
+    {
+        /// <summary>
+        /// Коллекция департаментов
+        /// </summary>
+        public ObservableCollection<Department> Departments;
+
+        /// <summary>
+        /// Содержит работников, не принадлежащих к какому-либо департаменту.
+        /// Поле Deletable этого департамента должно быть false; 
+        /// </summary>
+        public Department NoDepEmployees;
+        
+        /// <summary>
+        /// Конструктор без параметров.
+        /// </summary>
+        public Company()
+        {
+            Departments = new ObservableCollection<Department>();
+            NoDepEmployees = new Department("Сотрудники без департамента", false);
+            Departments.Add(NoDepEmployees);
+        }
+        
+        /// <summary>
+        /// Возвращает случайно сгенерированную компанию.
+        /// Для ДЗ
+        /// </summary>
+        /// <returns></returns>
+        public static Company GetRandomCompany()
+        {
+            Company tmpCompany = new Company();
+            var x = Util.GetRandom(5, 20);
+            for (int i = 0; i < x; i++)
+            {
+                tmpCompany.Departments.Add(Department.GetRandomDepartment());
+            }
+            return tmpCompany;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Departments).GetEnumerator();
+        }
+    }
+}
---
+++ 1st quarter/Продвинутый курс C#/Lesson_6_Kotenko/Lesson_5_Kotenko/Employee.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Lesson_5_Kotenko
+{
+    /// <summary>
+    /// Содержит параметры и логику сотрудника
+    /// </summary>
+    public class Employee :IEquatable<Employee>
+    {
+        /// <summary>
+        /// статический параметр для создания уникального имени
+        /// </summary>
+        public static int Index { get; private set; }
+        ~Employee()
+        {
+            Index--;
+        }
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        private int _age;
+        /// <summary>
+        /// Зарплата сотрудника
+        /// </summary>
+        private int _sel;
+        /// <summary>
+        /// Имя сотрудника
+        /// Не может быть пустой строкой
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value!=string.Empty)
+                {
+                    _name = value;
+                } 
+            }
+            
+        }        
+        /// <summary>
+        /// Возраст сотрудника. Не может быть меньше 0
+        /// </summary>
+        public int Age
+        {
+            get => _age;
+            set
+            {                
+                if ( value >0)
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Зарплата сотрудника. Не может быть меньше 0
+        /// </summary>
+        public int Sel
+        {
+            get => _sel;
+            set
+            {
+                if (value > 0)
+                {
+                    _sel = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Employee()
+        {
+            Index++;
+        }
+        /// <summary>
+        /// Возвращает сотрудника с рандомными параметрами
+        /// </summary>
+        public static Employee GetRandomEmployee()
+        {
+            return new Employee()
+            {
+                Name = "Simple Employee Name " + Index,
+                Age = Util.GetRandom(20, 50),
+                Sel = Util.GetRandom(40, 150) * 1000
+            };
+        }
+        /// <summary>
+        /// Создает сотрудника с заданными параметрами
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="sel">Зарплата</param>
+        public Employee(string name, int age, int sel)
+        {
+            Name = name;
+            Age = age;
+            Sel = sel;
+        }
+        /// <summary>
+        /// Сравнение двух сотрудников на равенство
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Employee other)
+        {
+            return other.Name == Name && 
+                   other.Age == Age &&
+                   other.Sel == Sel;
+        }
+    }
+}
