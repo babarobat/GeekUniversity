43930c87ae3e2df9cfdbd7a7fb972d2dab9f23ab
babarobat <36125153+babarobat@users.noreply.github.com>
2018-11-10 14:41:06 +0300

    123

--- 1st quarter/Продвинутый курс C#/Lesson_7_Kotenko/Lesson_7_kotenko/DataManager.cs
+++ 1st quarter/Продвинутый курс C#/Lesson_7_Kotenko/Lesson_7_kotenko/DataManager.cs
@@ -5,29 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.OleDb;
+using System.Data;
 
 namespace Lesson_7_kotenko
 {
     public static class DataManager
     {
         
+        static SqlConnection connection;
+        
+        
+
         public static void SaveEmployee(string path, string table,Employee emp)   
         {   
             try
             {
                 var sql = $@"INSERT INTO {table} (Name, Department, Age, Selary) 
                        VALUES ('{emp.Name}', '{emp.Dep}','{emp.Age.ToString()}', '{emp.Sel.ToString()}')";
-                using (SqlConnection connection = new SqlConnection(path))
+                using (connection = new SqlConnection(path))
                 {
                     connection.Open();
-
                     SqlCommand command = new SqlCommand(sql, connection);
                     command.ExecuteNonQuery();
                 }
             }
             catch (Exception e)
             {
-
                 Console.WriteLine(e.Message);
             }           
         }
@@ -37,7 +40,7 @@ namespace Lesson_7_kotenko
             {
                 var sql = $@"INSERT INTO {table} (Name) 
                        VALUES ('{dep.Name}')";
-                using (SqlConnection connection = new SqlConnection(path))
+                using (connection = new SqlConnection(path))
                 {
                     connection.Open();
 
@@ -51,5 +54,64 @@ namespace Lesson_7_kotenko
                 Console.WriteLine(e.Message);
             }
         }
+        public static DataTable GetDeps(string path)
+        {
+            try
+            {
+                using (connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    SqlCommand command = new SqlCommand("Select Distinct Department from Employees", connection);
+                    adapter.SelectCommand = command;
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }          
+        }
+        public static DataTable GetEmps(string path, string selectedDep)
+        {
+            try
+            {
+                using (connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    SqlCommand command = new SqlCommand($"SELECT Name, Age, Department, Selary FROM Employees WHERE Department = @Department", connection);
+                    command.Parameters.AddWithValue("Department", selectedDep);
+                    adapter.SelectCommand = command;
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    return dt;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        public static DataTable UpdateEmp(string path)
+        {
+            try
+            {
+                using (connection = new SqlConnection(path))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    SqlCommand command = new SqlCommand($"Update Employees SET Name = @Name, Age = @Age, Department = @Department, Selary = @Selary WHERE ID = @ID", connection);
+                    command.Parameters.AddWithValue("@Name",sql)
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
     }
 }
--- 1st quarter/Продвинутый курс C#/Lesson_7_Kotenko/Lesson_7_kotenko/MainWindow.xaml.cs
+++ 1st quarter/Продвинутый курс C#/Lesson_7_Kotenko/Lesson_7_kotenko/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +21,91 @@ namespace Lesson_7_kotenko
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = @"Data Source=(localdb)\MSSQLLocalDB;
+                            Initial Catalog = Lesson7;
+                            Integrated Security = True;
+                            Pooling = False";
         public MainWindow()
         {
             InitializeComponent();
+            #region Создание таблиц и загрузка в них данных
 
-            /// Саздание таблиц 
-            //string path = @"Data Source = (localdb)\MSSQLLocalDB;
-            //                Initial Catalog = Lesson7DB;
-            //                Integrated Security = True;
-            //                Pooling = False";
+
+            // Саздание таблицы сотрудников
+
+            // CREATE TABLE [dbo].[Employees]
+            //(            
+            //  [Id] INT IDENTITY(1, 1) NOT NULL,           
+            //  [Name] NVARCHAR(MAX) NOT NULL,
+            //  [Department] NVARCHAR(MAX) NULL, 
+            //  [Age] INT NULL,
+            //  [Selary] INT NULL
+            //  CONSTRAINT[PK_dbo.Employees] PRIMARY KEY CLUSTERED([Id] ASC)
+            // )
+
+            //Создание таблицы департаментов
+
+            //CREATE TABLE[dbo].[Departments]
+            //(
+            //[Id] INT IDENTITY(1, 1) NOT NULL,
+            //[Name] NVARCHAR(MAX) NULL
+            //CONSTRAINT[PK_dbo.Departments] PRIMARY KEY CLUSTERED([Id] ASC)
+            //)
+
+
+            //загрузка данных в таблицы
+
+            
 
             //DataCreater.CreateData(50, path, "Employees", "Departments");
+            #endregion
+            DepList.ItemsSource = DataManager.GetDeps(path).DefaultView; //DataTable
         }
 
+        private void DepList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            EmpList.ItemsSource = DataManager.GetEmps(path, (DepList.SelectedValue as DataRowView).Row[0].ToString()).DefaultView;
+        }
 
+        private void EmpList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EmpParamPannel.DataContext = EmpList.SelectedItem as DataRowView;
+        }
+
+        private void NameTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DeleteEmpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddEmpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddDepBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteDepBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DepName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void DepComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
