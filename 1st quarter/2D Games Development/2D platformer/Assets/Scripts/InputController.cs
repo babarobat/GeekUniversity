3ae5d0bf4c64c3d584c49cf676425f4e6072cbb7
babarobat <36125153+babarobat@users.noreply.github.com>
2018-12-23 21:50:24 +0300

    123

---
+++ 1st quarter/2D Games Development/2D platformer/Assets/Scripts/InputController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Game.Controllers
+{
+    /// <summary>
+    /// Класс для получения ввода пользователя
+    /// </summary>
+    class InputController : Singleton<InputController>
+    {
+        /// <summary>
+        /// Параметры ввода
+        /// </summary>
+        public ControlParams ControlParams { get; private set; }
+        protected override void Awake()
+        {
+            ControlParams = new ControlParams();
+
+        }
+        private void Update()
+        {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+            ControlParams.Horizontal = Input.GetAxisRaw("Horizontal");
+            ControlParams.Jump = Input.GetAxisRaw("Jump") > 0;
+            ControlParams.Fire = Input.GetAxisRaw("Fire1") > 0;
+            ControlParams.Interacting = Input.GetKeyDown(KeyCode.E);
+#endif
+
+
+        }
+        public void SetHor(float value)
+        {
+            ControlParams.Horizontal = value;
+            
+
+        }
+        public void SetFire(bool value)
+        {
+            ControlParams.Fire = value;
+            
+        }
+        public void SetJump(bool value)
+        {
+            ControlParams.Jump = value;
+            
+        }
+        public void SetInteracting(bool value)
+        {
+            ControlParams.Interacting = value;
+            
+
+        }
+        public void UP()
+        {
+            print(222);
+        }
+    }
+}
---
+++ 1st quarter/2D Games Development/2D platformer/Assets/TrailRendererController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRendererController : MonoBehaviour {
+    TrailRenderer tr;
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<TrailRenderer>();
+        pos = new Vector3(0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+#if UNITY_EDITOR
+        
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            pos.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            pos.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+            
+        }
+#endif
+
+#if UNITY_ANDROID
+        pos.x = Camera.main.ScreenToWorldPoint(Input.touches[0].position).x;
+        pos.y = Camera.main.ScreenToWorldPoint(Input.touches[0].position).y;
+        
+#endif
+        tr.transform.position = pos;
+    }
+}
